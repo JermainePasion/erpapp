ef80064495720a386dd849ca162679e86d9c3582
Jermaine_Riley Pasion <jmpasion@student.hau.edu.ph>
2023-09-13 16:30:16 +0800

    third commit

--- erpw/views.py
+++ erpw/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Jermaine Pasion',
+        'title': 'Post 1',
+        'content': 'Very First Post',
+        'date_posted': 'September 12, 2023',
+    },
+    {
+        'author': 'John John',
+        'title': 'Post 2',
+        'content': 'Another Post',
+        'date_posted': 'September 13, 2069',
+    }
+]
 def home (request):
-    return render (request, 'erpw/home.html')
+    context = {
+        'posts': posts
+    }
+    return render (request, 'erpw/home.html', context)
 
 def users (request):
-    return render (request, 'erpw/users.html')
+    return render (request, 'erpw/users.html', {'title':'Users'})
 
