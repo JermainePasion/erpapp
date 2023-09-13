4199d0501a91b30092ec958851fa37428eb02305
Jermaine_Riley Pasion <jmpasion@student.hau.edu.ph>
2023-09-13 09:38:21 +0800

    third commit

--- erpw/views.py
+++ erpw/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home (request):
-    return HttpResponse('<h1>Home page</h1>')
+    return render (request, 'erpw/home.html')
 
 def users (request):
-    return HttpResponse('<h1>Users Page</h1>')
+    return render (request, 'erpw/users.html')
 
