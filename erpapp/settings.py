f96d6ed1923946d9d7592f84784949eb780cfca3
Jermaine_Riley Pasion <jmpasion@student.hau.edu.ph>
2023-09-14 22:06:45 +0800

    September 14 commit

--- erpapp/settings.py
+++ erpapp/settings.py
@@ -32,12 +32,16 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'erpw.apps.ErpConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
@@ -118,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- erpw/views.py
+++ erpw/views.py
@@ -1,20 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        'author': 'Jermaine Pasion',
-        'title': 'Post 1',
-        'content': 'Very First Post',
-        'date_posted': 'September 12, 2023',
-    },
-    {
-        'author': 'John John',
-        'title': 'Post 2',
-        'content': 'Another Post',
-        'date_posted': 'September 13, 2069',
-    }
-]
+
 def home (request):
     return render (request, 'erpw/home.html')
 
