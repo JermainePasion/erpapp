de8e8b4ed7e088cc0812f3699e31bb24e17acc2f
Jermaine_Riley Pasion <jmpasion@student.hau.edu.ph>
2023-10-27 16:10:58 +0800

    October 27 Commit

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import Profile
 
 class UserLoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
@@ -45,3 +46,25 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    #email = forms.EmailField()
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        "class": "input",
+        "type": "email",
+    }))
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "input",
+        "type": "text",
+    }))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ['image']
