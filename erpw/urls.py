6b784095252af1699d1957c49f32e57538f9f3ad
Jermaine_Riley Pasion <jmpasion@student.hau.edu.ph>
2023-11-01 23:29:22 +0800

    November 1 Commit

--- erpw/urls.py
+++ erpw/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from . import views
+from .views import (PostListView,
+                    PostDetailView,
+                    PostCreateView,
+                    PostUpdateView,
+                    PostDeleteView
+)
 
 urlpatterns = [
     path('', views.home, name='erpw-dashboard'),
-    path('users/', views.users, name='erpw-users'),
+    path('users/', PostListView.as_view(), name='erpw-users'),
+    path('users/<int:pk>', PostDetailView.as_view(), name='erpw-detail'),
+    path('users/new/', PostCreateView.as_view(), name='erpw-create'),
+    path('users/<int:pk>/update/', PostUpdateView.as_view(), name='erpw-update'),
+    path('users/<int:pk>/delete/', PostDeleteView.as_view(), name='erpw-delete'),
 ]
--- erpw/views.py
+++ erpw/views.py
@@ -1,10 +1,66 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import UserPassesTestMixin
+
 
 @login_required
 def home (request):
     return render (request, 'inventory/dashboard.html')
+
+@method_decorator(login_required, name='dispatch')
+class PostListView(ListView):
+    model = Post
+    template_name = 'erpw/users.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+@method_decorator(login_required, name='dispatch')
+class PostDetailView(DetailView):
+    model = Post
+
+@method_decorator(login_required, name='dispatch')
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+@method_decorator(login_required, name='dispatch')
+class PostUpdateView(UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+@method_decorator(login_required, name='dispatch')
+class PostDeleteView(UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/users'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 @login_required
 def users (request):
