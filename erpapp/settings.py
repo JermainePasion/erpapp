c52aa5de2fc4e117207a76e8aa62ab73ae088084
Jermaine_Riley Pasion <jmpasion@student.hau.edu.ph>
2023-11-12 16:58:21 +0800

    November 12 Commit

--- erpapp/settings.py
+++ erpapp/settings.py
@@ -141,7 +141,7 @@ LOGIN_URL = 'login'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtop.gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = ''
