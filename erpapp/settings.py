ebd829bbcb038ea8f26b4b0d77a4f4c35aaae0f4
Jermaine_Riley Pasion <jmpasion@student.hau.edu.ph>
2023-11-14 20:17:01 +0800

    November 14 Commit 3

--- erpapp/settings.py
+++ erpapp/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
@@ -167,3 +168,4 @@ AWS_STORAGE_BUCKET_NAME = 'erpapp-files'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
